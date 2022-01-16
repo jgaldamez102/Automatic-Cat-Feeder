72dce7242abb00774a2125907c6770874525722f
Joses <jgaldamez102@gmail.com>
2022-01-15 20:46:36 -0800

    Initial commit, new file

---
+++ catFeeder.py
@@ -0,0 +1,20 @@
+""" 
+Automatic Cat Feeder
+
+Joses Galdamez - jgaldamez102@gmail.com
+
+"""
+
+import sys
+from datetime import datetime
+
+sys.path.append('home/pi/Dexter/GrovePi/Software/Python')
+
+
+
+def init():
+    #current time 
+    currTime = datetime.datetime.now()
+    return 
+if __name__ == "__main__":
+    init()
