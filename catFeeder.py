906083b14aba0c4aafc5ab3b5b649103e342df41
Joses <jgaldamez102@gmail.com>
2022-01-15 21:48:21 -0800

    Added some grovepi commands

--- catFeeder.py
+++ catFeeder.py
@@ -12,11 +12,25 @@ import grove_rgb_lcd as lcd
 
 sys.path.append('home/pi/Dexter/GrovePi/Software/Python')
 
-
+#initalizing Devices
+PORT_BUZZER = 2 #for D2 on the buzzer
+grovepi.pinMode(PORT_BUZZER, "OUTPUT")
 
 def init():
+    #initalize the display of the LCD
+    lcd.setRGB(224,255,255) # cool blue color
+    grovepi.digitalWrite(PORT_BUZZER, 0)# to initalize it turn off
     #current time 
-    currTime = datetime.datetime.now()
+    while True:
+        currTime = datetime.datetime.now()
+        currTimeHour = currTime.hour
+        currTimeMinute = currTime.minute
+        currTimeSecond = currTime.second
+        print(currTimeSecond)
+        # print(currTime)
+        #if the it hits a new
+        if (currTimeSecond == 0):
+            grovepi.digitalWrite(PORT_BUZZER, 1)
     return 
 if __name__ == "__main__":
     init()
