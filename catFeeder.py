234101dd2b7a9a2b7d56e3b37e9788f78dbda0aa
Joses <jgaldamez102@gmail.com>
2022-01-15 20:46:36 -0800

    Initial commit, new file

---
+++ catFeeder.py
@@ -0,0 +1,17 @@
+""" 
+Automatic Cat Feeder
+
+Joses Galdamez - jgaldamez102@gmail.com
+
+"""
+
+import sys
+import datetime
+
+
+
+def init():
+    print(datetime.datetime.now())
+    return 
+if __name__ == "__main__":
+    init()
