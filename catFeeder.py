fa6bad1a567f8435796f11ddc7fe230d5a37b204
Joses <jgaldamez102@gmail.com>
2022-01-17 17:04:50 -0800

    Adding stepper motor

--- catFeeder.py
+++ catFeeder.py
@@ -25,7 +25,7 @@ def init():
     control_pins = [7,11,13,15]
     for pin in control_pins:
         GPIO.setup(pin, GPIO.OUT)
-        GPIO.outpt(pin, 0)
+        GPIO.output(pin, 0)
     #current time 
     while True:
         currTime = datetime.datetime.now()
