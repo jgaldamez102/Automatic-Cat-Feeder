39b608cf8bb2dc311d921e4df75b4299808b9b71
Joses <jgaldamez102@gmail.com>
2022-01-17 17:04:33 -0800

    Adding stepper motor

--- catFeeder.py
+++ catFeeder.py
@@ -21,7 +21,11 @@ def init():
     #initalize the display of the LCD
     lcd.setRGB(224,255,255) # cool blue color
     grovepi.digitalWrite(PORT_BUZZER, 0)# to initalize it turn off
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD) #setting up the pins 
+    control_pins = [7,11,13,15]
+    for pin in control_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.outpt(pin, 0)
     #current time 
     while True:
         currTime = datetime.datetime.now()
@@ -39,6 +43,21 @@ def init():
         else:
             grovepi.digitalWrite(PORT_BUZZER, 0)
             lcd.setText_norefresh("Time: " + str(currTime)) 
+        halfstep_seq = [
+        [1,0,0,0],
+        [1,1,0,0],
+        [0,1,0,0],
+        [0,1,1,0],
+        [0,0,1,0],
+        [0,0,1,1],
+        [0,0,0,1],
+        [1,0,0,1]
+        ]
+        for i in range(512):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+
         
     return 
 if __name__ == "__main__":
