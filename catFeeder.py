e90b052e69add56757d94a768efeb46c80f3ca5d
Joses <jgaldamez102@gmail.com>
2022-01-17 17:13:36 -0800

    Adding stepper motor

--- catFeeder.py
+++ catFeeder.py
@@ -18,12 +18,29 @@ sys.path.append('home/pi/Dexter/GrovePi/Software/Python')
 PORT_BUZZER = 2 #for D2 on the buzzer
 grovepi.pinMode(PORT_BUZZER, "OUTPUT")
 
+def stepper():
+    control_pins = [7,11,13,15]
+    halfstep_seq = [
+        [1,0,0,0],
+        [1,1,0,0],
+        [0,1,0,0],
+        [0,1,1,0],
+        [0,0,1,0],
+        [0,0,1,1],
+        [0,0,0,1],
+        [1,0,0,1]
+        ]
+    for i in range(512):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+            # time.sleep(0.001)
+
 def init():
     #initalize the display of the LCD
     lcd.setRGB(224,255,255) # cool blue color
     grovepi.digitalWrite(PORT_BUZZER, 0)# to initalize it turn off
     GPIO.setmode(GPIO.BOARD) #setting up the pins 
-    control_pins = [7,11,13,15]
     for pin in control_pins:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, 0)
@@ -40,25 +57,11 @@ def init():
         #if the it hits a new
         if (currTimeSecond ==0):
             grovepi.digitalWrite(PORT_BUZZER, 1)
+            stepper() #calling stepper function
             lcd.setText("FEEDING TIME")
         else:
             grovepi.digitalWrite(PORT_BUZZER, 0)
             lcd.setText_norefresh("Time: " + str(currTime)) 
-        halfstep_seq = [
-        [1,0,0,0],
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,1,1,0],
-        [0,0,1,0],
-        [0,0,1,1],
-        [0,0,0,1],
-        [1,0,0,1]
-        ]
-        for i in range(512):
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-                # time.sleep(0.001)
 
         
     return 
